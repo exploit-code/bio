6a8e708d9d2b6fbb4a1e71ba2ac62bcda84a2bf3
Alexander <o_xx@list.ru>
2021-05-27 22:12:54 +0300

    change my name

--- src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
+++ src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
@@ -7,7 +7,7 @@ let AboutMe = () => {
     return (
         <div className='about_me'>
             <div className='about_me_text'>
-                <div className='sup_text'>Привет, меня зовут Александр.<br />
+                <div className='sup_text'>Привет, меня зовут Леха-Дарксир.<br />
                     Я HTML-верстальщик и начинающий frontend-разработчик.
                 </div>
                 <div className='sub_text'>Моё обучение никогда не останавливается, ведь мир не стоит на месте и всегда нужно сохранять актуальность своих знаний.</div>
