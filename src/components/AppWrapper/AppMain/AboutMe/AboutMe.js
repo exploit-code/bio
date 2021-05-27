d1dd377314ab189e898c7245a69157f55e628853
Alexander <o_xx@list.ru>
2021-05-28 00:00:59 +0300

    updated meta-info

--- src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
+++ src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
@@ -7,8 +7,7 @@ let AboutMe = () => {
     return (
         <div className='about_me'>
             <div className='about_me_text'>
-                <h1 className='sup_text'>Привет, меня зовут Александр.<br />
-                    Я HTML-верстальщик и начинающий frontend-разработчик.
+                <h1 className='sup_text'>Привет, меня зовут Александр.<br />Я HTML-верстальщик и начинающий Frontend-разработчик.
                 </h1>
                 <div className='sub_text'>Моё обучение никогда не останавливается, ведь мир не стоит на месте и всегда нужно сохранять актуальность своих знаний.</div>
                 <a href='https://t.me/E_xploit' target='// eslint-disable-next-line _blank'>
