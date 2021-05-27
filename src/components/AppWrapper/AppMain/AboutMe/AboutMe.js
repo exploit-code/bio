bef39f6dbc3d851353ea55acc5a01ab907913cf8
Alexander <o_xx@list.ru>
2021-05-27 22:42:11 +0300

    new font

--- src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
+++ src/components/AppWrapper/AppMain/AboutMe/AboutMe.js
@@ -7,9 +7,9 @@ let AboutMe = () => {
     return (
         <div className='about_me'>
             <div className='about_me_text'>
-                <div className='sup_text'>Привет, меня зовут Александр.<br />
+                <h1 className='sup_text'>Привет, меня зовут Александр.<br />
                     Я HTML-верстальщик и начинающий frontend-разработчик.
-                </div>
+                </h1>
                 <div className='sub_text'>Моё обучение никогда не останавливается, ведь мир не стоит на месте и всегда нужно сохранять актуальность своих знаний.</div>
                 <a href='https://t.me/E_xploit' target='// eslint-disable-next-line _blank'>
                     <Button text={'Напиши мне!'} />
